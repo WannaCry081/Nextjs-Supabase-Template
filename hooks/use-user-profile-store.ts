64e11ab54c438dfa4787b4dcead5ede365aba2e9
Ly <liraedata59@gmail.com>
2026-01-12 01:59:05 +0800

    feat: add custom hook useUserProfileStore for accessing user profile state

---
+++ hooks/use-user-profile-store.ts
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { useStore } from "zustand";
+
+import {
+  UserProfileContext,
+  type UserProfileStore,
+} from "@/components/providers/user-profile-provider";
+
+export const useUserProfileStore = <T>(
+  selector: (state: ReturnType<UserProfileStore["getState"]>) => T
+) => {
+  const store = useContext(UserProfileContext);
+
+  if (!store) {
+    throw new Error("`useUserProfileStore` must be used within a UserProfileProvider");
+  }
+
+  return useStore(store, selector);
+};
