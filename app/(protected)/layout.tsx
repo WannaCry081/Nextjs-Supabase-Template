4d8e29debd4b37124b353e734579d66985b4ae3d
Ly <liraedata59@gmail.com>
2026-01-12 01:59:11 +0800

    feat: wrap layout in UserProfileProvider for user profile context

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -5,16 +5,21 @@ import { AppSidebar } from "@/components/app-sidebar";
 import { SiteHeader } from "@/components/app-sidebar/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
+// Providers
+import { UserProfileProvider } from "@/components/providers/user-profile-provider";
+
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <div className="[--header-height:calc(--spacing(14))]">
-      <SidebarProvider className="flex flex-col">
-        <SiteHeader />
-        <div className="flex flex-1">
-          <AppSidebar />
-          <SidebarInset>{children}</SidebarInset>
-        </div>
-      </SidebarProvider>
-    </div>
+    <UserProfileProvider>
+      <div className="[--header-height:calc(--spacing(14))]">
+        <SidebarProvider className="flex flex-col">
+          <SiteHeader />
+          <div className="flex flex-1">
+            <AppSidebar />
+            <SidebarInset>{children}</SidebarInset>
+          </div>
+        </SidebarProvider>
+      </div>
+    </UserProfileProvider>
   );
 }
