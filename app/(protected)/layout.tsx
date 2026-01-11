fec863a89db6397809de083420c212c5490a2a12
Ly <liraedata59@gmail.com>
2026-01-12 01:08:30 +0800

    feat: implement PageClient and Layout components for dashboard structure

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,5 +1,20 @@
 import { PropsWithChildren } from "react";
 
+// Components
+import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/app-sidebar/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
 export default function Layout({ children }: PropsWithChildren) {
-  return <>{children}</>;
+  return (
+    <div className="[--header-height:calc(--spacing(14))]">
+      <SidebarProvider className="flex flex-col">
+        <SiteHeader />
+        <div className="flex flex-1">
+          <AppSidebar />
+          <SidebarInset>{children}</SidebarInset>
+        </div>
+      </SidebarProvider>
+    </div>
+  );
 }
