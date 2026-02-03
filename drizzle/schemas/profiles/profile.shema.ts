6b159f7951d1f7818589df185690276d05570856
Ly <liraedata59@gmail.com>
2026-02-04 00:43:58 +0800

    chore: remove unused profile schema file

--- drizzle/schemas/profiles/profile.shema.ts
+++
@@ -1,9 +0,0 @@
-import { pgTable, varchar } from "drizzle-orm/pg-core";
-import { baseColumns } from "../base";
-
-export const profiles = pgTable("profiles", {
-  ...baseColumns,
-  email: varchar("email", { length: 255 }).notNull(),
-  name: varchar("name", { length: 100 }).notNull(),
-  imageUrl: varchar("image_url", { length: 255 }),
-});
