3b54760e8deec9d3fdfa3a357a936804003e9e0f
Ly <liraedata59@gmail.com>
2026-02-04 01:19:50 +0800

    feat: add NavSecondary component for sidebar navigation

---
+++ components/app-sidebar/nav-secondary.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import type { LucideIcon } from "lucide-react";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+interface NavSecondaryProps extends React.ComponentPropsWithoutRef<typeof SidebarGroup> {
+  items: {
+    name: string;
+    url: string;
+    icon: LucideIcon;
+  }[];
+}
+
+export const NavSecondary = ({ items, ...props }: NavSecondaryProps) => {
+  return (
+    <SidebarGroup {...props}>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.name}>
+              <SidebarMenuButton asChild size="sm">
+                <a href={item.url}>
+                  <item.icon />
+                  <span>{item.name}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
