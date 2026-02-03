06a27d787bd2852a6d839c827f0295e0489d07b7
Ly <liraedata59@gmail.com>
2026-02-04 01:00:00 +0800

    chore: add SEO constants for site metadata

---
+++ constants/seo.constant.ts
@@ -0,0 +1,5 @@
+export const SITE_NAME = "Next.js Supabase Template";
+export const SITE_URL = "http://localhost:3000/";
+
+export const DEFAULT_TITLE = "Next.js Supabase Template";
+export const DEFAULT_DESCRIPTION = "A batteries-included Next.js + Supabase starter.";
