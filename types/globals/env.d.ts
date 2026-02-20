801f838435827b031855894eca4b7445351b159f
Ly <liraedata59@gmail.com>
2026-02-20 14:42:19 +0800

    fix: add NEXT_PUBLIC_SITE_URL to ProcessEnv interface for environment variable support

--- types/globals/env.d.ts
+++ types/globals/env.d.ts
@@ -4,6 +4,7 @@ namespace NodeJS {
 
     NEXT_PUBLIC_SUPABASE_URL: string;
     NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: string;
+    NEXT_PUBLIC_SITE_URL: string;
 
     RESEND_API_KEY: string;
     RESEND_EMAIL_FROM: string;
